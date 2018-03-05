b6d53a95b71e37e7cb0cf0db8e4c0a8817c17810
rob.a.berger@gmail.com Berger <rob.a.berger@gmail.com>
2018-03-05 22:32:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = [];
 var katzDeliLine = 0;
 
 function takeANumber(katzDeliLine, name) {
-  katzDeli[] = name;
+  katzDeli = name;
   return "Welcome, " + name + ". You are number " + katzDeli.length + " in line.";
 }
 
