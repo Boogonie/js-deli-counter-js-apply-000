1496c66fefa06783185f50a4ec15f2f1abad1e76
rob.a.berger@gmail.com Berger <rob.a.berger@gmail.com>
2018-03-05 22:44:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function takeANumber(katzDeliLine, name) {
 
 function nowServing() {
   if (typeof katzDeliLine !== 'undefined' && katzDeliLine.length > 0) {
-    
+    return katzDeliLine[0];
   } else {
     return "There is nobody waiting to be served!"
   }
